2ab69b7740813113627e71202b831ea81607f82d
Edmund <edmund.a.berry@cern.ch>
2014-03-18 14:18:12 +0100

    Saved commands: initial commit

---
+++ saved_commands/launch_tpg_mc.sh
@@ -0,0 +1,8 @@
+python scripts/launch_analysis.py \
+-c ../HcalTPG/HcalTPGAnalyzer/test/hcaltpganalyzer_MC_cfg.py \
+-w $PWD/work/TPG_MC_V01-00-00 \
+-n 350 \
+-q 1nh \
+-i file_lists/TPG_MC/inputListAll.txt \
+-o TPG_MC_V01-00-00 \
+-t START53_V7N
---
+++ saved_commands/make_list_tpg_data.sh
@@ -0,0 +1,1 @@
+python scripts/make_file_list.py -i /eos/cms/store/data/Run2012C/MinimumBias/RAW-RECO/25Feb2013-v1/10000/ -o file_lists/TPG_Data/
