6b7f1a80d9c94618ce8ef4f6aee455afcfba3257
Edmund <edmund.a.berry@cern.ch>
2014-04-10 13:37:01 -0500

    initial commit

---
+++ saved_commands/HcalReco/launch_reco_mc.sh
@@ -0,0 +1,9 @@
+python scripts/launch_analysis.py \
+-c hcalNoise_cfg.py \
+-w $PWD/work/HcalNoise_MinBias_MC \
+-n 1 \
+-q 1nh \
+-i file_lists/MinBias_TuneZ2star_13TeV/inputListAll_1.txt \
+-o HcalNoise_MinBias_MC \
+-t "auto:mc" \
+-e /eos/uscms/store/user/eberry/MinBiasMC/
