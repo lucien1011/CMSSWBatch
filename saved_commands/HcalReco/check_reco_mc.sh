47666cd53a171db5d05bf4507c48951464c00b66
Edmund <edmund.a.berry@cern.ch>
2014-04-10 13:47:55 -0500

    change from test to production

--- saved_commands/HcalReco/check_reco_mc.sh
+++ saved_commands/HcalReco/check_reco_mc.sh
@@ -1,9 +1,9 @@
 python scripts/check_output.py \
--c hcalNoise_cfg.py \
+-c ../HcalReco/test/hcalNoise_fromGEN-SIM_cfg.py \
 -w $PWD/work/HcalNoise_MinBias_MC \
--n 1 \
+-n 500 \
 -q 1nh \
--i file_lists/MinBias_TuneZ2star_13TeV/inputListAll_1.txt \
+-i file_lists/MinBias_TuneZ2star_13TeV/inputListAll.txt \
 -o HcalNoise_MinBias_MC \
 -t "auto:mc" \
 -e /eos/uscms/store/user/eberry/MinBiasMC/
