ac00beb1d7b272b9e0014dbe25238ccb57105648
Edmund <edmund.a.berry@cern.ch>
2014-04-14 11:14:29 -0500

    QCD 1800 commands

---
+++ saved_commands/HcalReco/make_list_reco_mc_QCD1800.sh
@@ -0,0 +1,5 @@
+python scripts/make_file_list.py \
+-i /pnfs/cms/WAX/11/store/mc/Fall13/QCD_Pt-1800_TuneZ2star_13TeV_pythia6/GEN-SIM/POSTLS162_V1-v1/30000/ \
+-o file_lists/QCD_Pt-1800_TuneZ2star_13TeV
+
+
