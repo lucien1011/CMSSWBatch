f65e7f1ee416b858ee47aff972122f7e39bfeb5e
Edmund <edmund.a.berry@cern.ch>
2014-04-14 20:29:42 -0500

    Added command for checking QCD MC

---
+++ saved_commands/HcalReco/check_reco_mc_QCD1800.sh
@@ -0,0 +1,9 @@
+python scripts/check_output.py \
+-c ../HcalReco/test/hcalNoise_fromGEN-SIM_62X_cfg.py \
+-w $PWD/work/HcalNoise_QCD1800_MC \
+-n 500 \
+-q 1nh \
+-i file_lists/QCD_Pt-1800_TuneZ2star_13TeV/inputListAll.txt \
+-o HcalNoise_QCD1800_MC \
+-t "auto:mc" \
+-e /eos/uscms/store/user/eberry/QCD1800MC/
