88776f5bd405caa3848b9c6a9f1c22d0b65ba78f
Edmund <edmund.a.berry@cern.ch>
2014-04-23 09:32:17 -0500

    New saved commands for adding pileup to QCD MC

---
+++ saved_commands/HcalReco/check_reco_mc_QCD1800_GEN-SIM-RAW.sh
@@ -0,0 +1,8 @@
+python scripts/check_output.py \
+-c ../HcalReco/test/hcalNoise_fromGEN-SIM_toGEN-SIM-RAW_62X_withMixer_cfg.py \
+-w $PWD/work/HcalNoise_QCD1800_GEN-SIM-RAW_MC \
+-n 500 \
+-q 1nh \
+-o HcalNoise_QCD1800_GEN-SIM-RAW_MC \
+-t "auto:mc" \
+-e /eos/uscms/store/user/eberry/QCD1800MC_GEN-SIM-RAW/
