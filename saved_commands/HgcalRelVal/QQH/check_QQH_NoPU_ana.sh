25787b114e6382be057c9b5d4ce5067c2c0d741c
Edmund <edmund.a.berry@cern.ch>
2015-01-07 16:32:30 +0100

    Updated for SLHC22

---
+++ saved_commands/HgcalRelVal/QQH/check_QQH_NoPU_ana.sh
@@ -0,0 +1,8 @@
+python scripts/check_output.py \
+-c ../HGCALANA/HgcalTupleMaker/analysis_cfg.py \
+-w $PWD/work/HgcalRelVal_QQH_NoPU_ANA \
+-n 500 \
+-q 8nh \
+-o HgcalRelVal_QQH_NoPU_MC_ANA \
+-t "auto:mc" \
+-e /eos/cms/store/user/eberry/HGCAL/HgcalRelVal_QQH_NoPU_MC_SLHC22_ANA/
