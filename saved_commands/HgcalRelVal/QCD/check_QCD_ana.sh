3d7f722413d248621b0cc3fa3ad5592bc75fe977
Edmund <edmund.a.berry@cern.ch>
2015-03-22 16:52:01 +0100

    Updated release and pandora usage

--- saved_commands/HgcalRelVal/QCD/check_QCD_ana.sh
+++ saved_commands/HgcalRelVal/QCD/check_QCD_ana.sh
@@ -1,8 +1,8 @@
 python scripts/check_output.py \
--c ../HGCALANA/HgcalTupleMaker/analysis_cfg.py \
+-c ../HGCALANA/HgcalTupleMaker/analysis_pandora_cfg.py \
 -w $PWD/work/HgcalRelVal_QCD_ANA \
 -n 500 \
 -q 8nh \
 -o HgcalRelVal_QCD_MC_ANA \
 -t "auto:mc" \
--e /eos/cms/store/user/eberry/HGCAL/HgcalRelVal_QCD_MC_SLHC23patch2_ANA/
+-e /eos/cms/store/user/eberry/HGCAL/HgcalRelVal_QCD_MC_SLHC25_patch1_ANA/
--- saved_commands/HgcalRelVal/QCD/launch_QCD_ana.sh
+++ saved_commands/HgcalRelVal/QCD/launch_QCD_ana.sh
@@ -1,9 +1,9 @@
 python scripts/launch_analysis.py \
--c ../HGCALANA/HgcalTupleMaker/analysis_cfg.py \
+-c ../HGCALANA/HgcalTupleMaker/analysis_pandora_cfg.py \
 -w $PWD/work/HgcalRelVal_QCD_ANA \
 -n 500 \
 -q 8nh \
 -i file_lists/HgcalRelVal_QCD_RECO/inputListAll.txt \
 -o HgcalRelVal_QCD_MC_ANA \
 -t "auto:mc" \
--e /eos/cms/store/user/eberry/HGCAL/HgcalRelVal_QCD_MC_SLHC23patch2_ANA/
+-e /eos/cms/store/user/eberry/HGCAL/HgcalRelVal_QCD_MC_SLHC25_patch1_ANA/
