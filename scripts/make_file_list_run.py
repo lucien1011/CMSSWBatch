9e2794374f5bf2b1673ea4d3e6798b0a65ddf96a
lucien1011 <lucien1011@gmail.com>
2015-09-25 13:47:08 +0200

    run number and pd from command line

--- scripts/make_file_list_run.py
+++ scripts/make_file_list_run.py
@@ -1,19 +1,19 @@
 
 from DBHandler import getFilesFromRun
+import argparse
+
+parser = argparse.ArgumentParser(description='Make an input list')
+
+parser.add_argument('-r', metavar="runList", dest='runList',action="store" , required=True, help='the run number list',type=str)
+
+parser.add_argument('-p', metavar="PD", dest='dataset',action="store" , required=True, help='the PD',type=str)
+parser.add_argument('-o', metavar="output", dest='output_folder',action="store" , required=True, help='output folder where input list will be stored')
+
+args = parser.parse_args()
 
 outPath = "/afs/cern.ch/work/k/klo/hcal/PromptAnalysis/CMSSW_7_4_10/src/batch/file_list/Collision_HF5050_ZeroBias/inputListAll.txt"
-# PD = "/ZeroBias/Run2015C-v1/RAW"
-PD = "/ZeroBias/Run2015D-v1/RAW"
-runList = [
-256004,
-256217,
-256353,
-256464,
-256677, 
-256843, 
-256936,
-257058,
-]
+PD = args.dataset 
+runList = map(int,args.runList.split(","))
 
 filePathList = []
 for run in runList:
@@ -22,7 +22,7 @@ for run in runList:
 	filePathList += tempList
 	print "Number of files for this run: %s"%len(tempList)
 
-file = open(outPath,"w")
+file = open(outPath+"inputListAll.txt","w")
 for filePath in filePathList:
 	file.write("\""+filePath+"\",\n")
 file.close()
