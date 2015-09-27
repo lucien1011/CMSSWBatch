85c2c07671a746099574bc748d83190457dfcada
lucien1011 <lucien1011@gmail.com>
2015-09-27 18:35:50 +0200

    added run step and run range functionality, print out in the end

--- scripts/make_file_list_run.py
+++ scripts/make_file_list_run.py
@@ -1,31 +1,63 @@
 
 from DBHandler import getFilesFromRun
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='Make an input list')
 
 parser.add_argument('-r', metavar="runList", dest='runList',action="store" , required=True, help='the run number list',type=str)
+parser.add_argument('-s', metavar="runstep", dest='runStep',action="store" , required=False, help='the run number step',type=int)
 
 parser.add_argument('-p', metavar="PD", dest='dataset',action="store" , required=True, help='the PD',type=str)
-parser.add_argument('-o', metavar="output", dest='output_folder',action="store" , required=True, help='output folder where input list will be stored')
+parser.add_argument('-o', metavar="output", dest='output_folder',action="store" , required=True, help='output folder where input list will be stored',type=str)
 
 args = parser.parse_args()
+outdir = args.output_folder
+PD = args.dataset
 
-outPath = "/afs/cern.ch/work/k/klo/hcal/PromptAnalysis/CMSSW_7_4_10/src/batch/file_list/Collision_HF5050_ZeroBias/inputListAll.txt"
-PD = args.dataset 
-runList = map(int,args.runList.split(","))
+if "-" in args.runList:
+	tempList = args.runList.split("-")
+	if len(tempList) != 2:
+		raise RuntimeError, "Invalid format for run range"
+	if not hasattr(args,"runStep"):
+		raise RuntimeError, "Please provide the run step"
+	else:
+		runStep = args.runStep
+	lowerRange = int(args.runList.split("-")[0])
+	upperRange = int(args.runList.split("-")[1])
+	runList = range(lowerRange,upperRange,runStep)
+else:
+	runList = map(int,args.runList.split(","))
 
 filePathList = []
+validRunList = []
 for run in runList:
 	print "Getting files for run %s"%run
 	tempList = getFilesFromRun(PD,run)
 	filePathList += tempList
 	print "Number of files for this run: %s"%len(tempList)
+	if (len(tempList) != 0):
+		validRunList.append(run)
 
-file = open(outPath+"inputListAll.txt","w")
+if not os.path.isdir ( outdir ):
+	os.system ( "mkdir -p " + outdir )
+
+file = open(outdir+"/inputListAll.txt","w")
 for filePath in filePathList:
 	file.write("\""+filePath+"\",\n")
 file.close()
+
+validRunList.sort()
+print "==========================================="
+print "------------------Summary------------------"
+print "==========================================="
+print "Total number of required runs: %s"%len(runList)
+print "Total number of valid runs: %s"%len(validRunList)
+runString = ""
+for validRun in validRunList:
+	runString += "%s "%validRun
+print "List of valid runs:"
+print runString
 
 
 
